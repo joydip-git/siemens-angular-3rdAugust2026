57740fbe071adc2fb9639de4ede45519ae74aa0f
joydip-git <joydip.mondal@gmail.com>
2026-08-06 16:44:18 +0530

    form handling-Login component code created

--- codes/day-4/pms-app-v3-routing/src/app/modules/auth/components/login/login.ts
+++ codes/day-4/pms-app-v3-routing/src/app/modules/auth/components/login/login.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  private builder = inject(FormBuilder)
+  
+  loginForm = this.builder.group({
+    username: ['enter user name', [Validators.required, Validators.email]],
+    password: ['enter password', Validators.required]
+  })
 
+  get username() {
+    return this.loginForm.get('username')
+  }
+  get password() {
+    return this.loginForm.get('password')
+  }
+
+  submit() {
+    
+  }
 }
